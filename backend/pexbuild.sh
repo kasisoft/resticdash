0ada87bd22edccc286ae817d12bd90a19173df82
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-02-26 18:12:22 +0100

    ~ installing virtualenv if required

--- backend/pexbuild.sh
+++ backend/pexbuild.sh
@@ -3,6 +3,7 @@ export DIRNAME=$(readlink -f  $(dirname $0))
 export CURRENTDIR=$(pwd)
 cd "${DIRNAME}"
 if [ ! -d "${DIRNAME}/venv" ]; then
+    python3 -m pip install --user virtualenv
     virtualenv venv
 fi
 source "./venv/bin/activate"
