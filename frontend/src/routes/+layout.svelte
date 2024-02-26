3fcad91c9363d902f39c41d7bd557ecae17c9e35
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-02-26 04:18:22 +0100

    ~ frontend: specified a basic layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,38 @@
-<script>
-	import '../app.pcss';
+<script lang="ts">
+    import '$src/styles.pcss';
+    export let data;
 </script>
 
-<slot />
+<div class="app content-grid">
+    <header class="full-width content-grid">
+        <Header />
+    </header>
+    <main class="full-width content-grid">
+        <slot />
+    </main>
+    <footer class="full-width content-grid">
+        <Footer />
+    </footer>
+</div>
+
+
+<style lang="postcss">
+    .app {
+        min-height: 100vh;
+        min-height: 100svh;
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+    }
+    header {
+        grid-template-rows: var(--header-height) auto;
+        background-color: theme('colors.primary.300');
+    }
+    main {
+        height: 100%;
+        width: 100%;
+        background-color: theme('colors.primary.100');
+    }
+    footer {
+        background-color: theme('colors.primary.300');
+    }
+</style>
