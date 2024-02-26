11cbeda7223723644c93d30b310d3338d9184e6f
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-02-26 04:16:34 +0100

    + frontend: added a basic 'Header' and 'Footer' component

---
+++ frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,13 @@
+<div class="pb-4 pt-4 flex">
+    <a class="font-semibold underline hover:no-underline text-primary-600" href="https://kasisoft.com" target="_blank">https://kasisoft.com</a>
+    <a class="font-semibold underline hover:no-underline text-primary-600" href="mailto:daniel.kasmeroglu@kasisoft.com">daniel.kasmeroglu@kasisoft.com</a>
+</div>
+
+
+<style lang="postcss">
+    div {
+        align-items: center;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+</style>
---
+++ frontend/src/lib/components/Header.svelte
@@ -0,0 +1,11 @@
+<div class="flex">
+    <svg  xmlns="http://www.w3.org/2000/svg">
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="60" fill="#FF7F50">ResticDash</text>
+    </svg>
+</div>
+
+<style lang="postcss">
+    div {
+        justify-content: center;
+    }
+</style>
