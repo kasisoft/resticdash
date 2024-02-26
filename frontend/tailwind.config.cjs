d8b97194155ed2563710cde7cc78ab73946b11bc
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-02-26 04:09:03 +0100

    ~ frontend: flowbite integration

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config}*/
 const config = {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-
+    content: [
+        './src/**/*.{html,js,svelte,ts}',
+        './node_modules/flowbite/**/*.js'
+    ],
+    plugins: [
+        require('flowbite/plugin')
+    ],
+    darkMode: 'class',
+    /* https://tailwindcss.com/docs/theme */
     theme: {
-        extend: {}
+        extend: {
+        }
     },
-
-    plugins: []
 };
 
 module.exports = config;
