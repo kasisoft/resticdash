7a85e564310523f10f5ae4417880903f509442e7
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-02-26 04:24:00 +0100

    + frontend: added type definitions for the responses of the backend

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,3 +1,36 @@
+import { ark, arrayOf, type } from "arktype";
+
+export const FrontendCfgDef = type({
+    "reload": "number",
+    "timeout": "number"
+});
+
+export type FrontendCfg = typeof FrontendCfgDef.infer;
+
+export const DEFAULT_FRONTENDCFG: FrontendCfg = {
+    reload: 60,
+    timeout: 10
+};
+
+export const SnapshotInfoDef = type({
+    "id": "string",
+    "short_id": "7<string<9",
+    "time": ["string", "|>", ark.parsedDate],
+});
+
+export type SnapshotInfo = typeof SnapshotInfoDef.infer;
+
+export const BackupInfosDef = type({
+    "name": "0<string<100",
+    "backup_fail_delay": "0<integer<=1000000000",
+    "snapshots": arrayOf(SnapshotInfoDef),   // oldest = 0, newest = length - 1
+});
+
+export type BackupInfos = typeof BackupInfosDef.infer;
+
+export const BackupInfosListDef = arrayOf(BackupInfosDef);
+
+export type BackupInfosList = typeof BackupInfosListDef.infer;
 
 export enum ApplicationState {
 
