2ae929c104d125af179155cd48b522e36eea2268
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-02-26 22:40:03 +0100

    ~ fix: making sure that the destination directories exist

--- fs/random-files.sh
+++ fs/random-files.sh
@@ -2,6 +2,9 @@
 
 . ./common.sh
 
+mkdir -p "${DIRNAME}/${SOURCEDIRS[0]}"
+mkdir -p "${DIRNAME}/${SOURCEDIRS[1]}"
+
 dd if=/dev/urandom of="${DIRNAME}/${SOURCEDIRS[0]}/file_a.bin" bs=1024 count=1
 
 dd if=/dev/urandom of="${DIRNAME}/${SOURCEDIRS[1]}/file_a.bin" bs=1024 count=2
