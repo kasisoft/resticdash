5d5cb9d6008e83f7d394be0a545a6c4508c50396
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-02-26 04:36:02 +0100

    - frontend: changed back arktype version as the most recent one doesn't work for now

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -14,10 +14,10 @@ const config = {
         adapter: adapter(),
 
         alias: {
-            $lib: './src/lib',
-            $i18n: './src/i18n',
-            $assets: './src/assets',
-            $src: './src'
+            '$lib': './src/lib',
+            '$i18n': './src/i18n',
+            '$assets': './src/assets',
+            '$src': './src'
         }
     }
 };
