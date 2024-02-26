550367c6861b7ab0f0300d3b72686457577999d5
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-02-26 03:57:09 +0100

    ~ frontend: formatting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [
+        sveltekit()
+    ],
     test: {
-        include: ['src/**/*.{test,spec}.{js,ts}']
+        include: [
+            'src/**/*.{test,spec}.{js,ts}'
+        ]
     }
 });
