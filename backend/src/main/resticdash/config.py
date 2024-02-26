6a5753512de81c49784f7d9833deeca763c0dbdc
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-02-26 01:55:48 +0100

    + backend: added a settings object with a pidfile

--- backend/src/main/resticdash/config.py
+++ backend/src/main/resticdash/config.py
@@ -1,9 +1,25 @@
-from dataclasses_json import dataclass_json
 from dataclasses import dataclass
+from typing import Optional
+
+from dataclasses_json import dataclass_json
+
+
+@dataclass_json
+@dataclass
+class CfgSettings:
+
+    # Location of the PID file for resticdash
+    pidfile: str = '/var/run/resticdash.pid'
 
 
 @dataclass_json
 @dataclass
 class CfgResticDash:
 
+    settings: Optional[CfgSettings] = None
     version: int = 1
+
+    def __post_init__(self):
+
+        if self.settings is None:
+            self.settings = CfgSettings()
