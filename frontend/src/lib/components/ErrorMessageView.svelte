e8317426e710fbacb1abe2e5c500bb3ac850c929
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-03-27 14:51:52 +0100

    ~ replaced the flowbite code for the alert by a Svelte Alert component now

--- frontend/src/lib/components/ErrorMessageView.svelte
+++ frontend/src/lib/components/ErrorMessageView.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	
+
+    import { Alert } from 'svelte-5-ui-lib';
+    import { InfoCircleSolid } from 'flowbite-svelte-icons';
+
     import { alertStore } from '$lib/stores.svelte';
     import { _ } from 'svelte-i18n';
 
@@ -10,22 +13,10 @@
 </script>
 
 {#if alertStore.changed}
-    <section class="full-width p-4 text-sm text-red-800 bg-red-50 border-red-600 border-y-2 dark:bg-gray-800 dark:text-red-400" role="alert">
-        <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-          </svg>
+    <Alert divclass="border-t-4 border-b-4 full-width bg-red-50 text-red-800 border-red-600 border-y-2" dismissable onclick={dismiss}>
+        {#snippet icon()}
+            <InfoCircleSolid class="w-5 h-5" />
+        {/snippet}
         <span class="font-medium">{$_('general.error')}:</span>{alertStore.value}
-        <button on:click={dismiss} type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700" aria-label="Close">
-            <span class="sr-only">Close</span>
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-            </svg>
-          </button>
-    </section>
+    </Alert>
 {/if}
-
-<style lang="postcss">
-    section {
-        display: flex;
-    }
-</style>
