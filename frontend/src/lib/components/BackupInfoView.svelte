ceedbb84834c3dfb46b0fd7e088bbff267ee1833
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-03-27 23:26:40 +0100

    ~ replaced 'flowbite' by svelte-5-ui-lib now

--- frontend/src/lib/components/BackupInfoView.svelte
+++ frontend/src/lib/components/BackupInfoView.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 
-    import type { BackupInfos, SnapshotInfo } from '$lib/types';
+    import { Card } from 'svelte-5-ui-lib';
     import { _ } from 'svelte-i18n';
+
+    import type { BackupInfos, SnapshotInfo } from '$lib/types';
 
     let { backupInfo } = $props<{ backupInfo: BackupInfos }>();
 
@@ -15,62 +17,51 @@
             const millis     = now.getTime() - lastBackup.getTime();
             const seconds    = millis / 1000;
             failed           = seconds >= backupInfo.backup_fail_delay;
-            snapshotInfos.push(backupInfo.snapshots[0]);
             snapshotInfos.push(backupInfo.snapshots[backupInfo.snapshots.length - 1]);
+            snapshotInfos.push(backupInfo.snapshots[0]);
         } else {
             // no snapshots is considered to be an error
             failed = true;
         }
     }
 
-    const colorClass = failed ? "bg-red-200" : "bg-green-200";
+    const colorClass = failed ? "bg-red-200 dark:bg-red-200" : "bg-green-300 dark:bg-green-300";
 
 </script>
 
-<div>
-    <a href="#" class="{colorClass} block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{backupInfo.name}</h5>
-        <p class="font-normal mb-2 text-gray-700 dark:text-gray-400">{$_('backup.snapshots')}: {backupInfo.snapshots.length}</p>
-        <table>
-            <thead>
-                <tr>
-                    <th class="date">{$_('backup.date')}</th>
-                    <th class="time">{$_('backup.time')}</th>
-                    <th class="shortid">{$_('backup.short_id')}</th>
-                </tr>
-            </thead>
-            <tbody>
-                {#each snapshotInfos as snapshot, j}
-                <tr>
-                    <td>{snapshot.time.toLocaleDateString()}</td>
-                    <td>{snapshot.time.toLocaleTimeString()}</td>
-                    <td>{snapshot.short_id}</td>
-                </tr>
-                {/each}
-            </tbody>
-        </table>
-    </a>
-</div>
+<Card href="./backups/{backupInfo.name}" class="{colorClass} border-primary-500 dark:border-primary-200 hover:bg-gray-100 dark:hover:bg-gray-100">
+    <h5 class="bv mb-2 text-2xl font-bold tracking-tight">{backupInfo.name}</h5>
+    <p class="bv font-semibold mb-2 ">{$_('backup.snapshots')}: {backupInfo.snapshots.length}</p>
+    <table class="bv">
+        <thead>
+            <tr>
+                <th>{$_('backup.date')}</th>
+                <th>{$_('backup.time')}</th>
+                <th>{$_('backup.short_id')}</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each snapshotInfos as snapshot, j}
+            <tr>
+                <td>{snapshot.time.toLocaleDateString()}</td>
+                <td>{snapshot.time.toLocaleTimeString()}</td>
+                <td>{snapshot.short_id}</td>
+            </tr>
+            {/each}
+        </tbody>
+    </table>
+</Card>
 
 <style lang="postcss">
-    div {
-        height: 12rem;
-        > a {
-            height: 100%;
-        }
-    }
-    .date {
+    th {
+        text-align: left;
         width: 10rem;
-        text-align: left;
     }
-    .time {
-        width: 9rem;
-        text-align: left;
+    /*
+     * @todo [27-MAR-2024:KASI]  Matching via classes doesn't seem to work right. Figure out why?
+     * :global etc didn't work or I made an error!
+     */
+    .bv {
+        color: theme('colors.primary.700');
     }
-    .shortid {
-        width: 9rem;
-        text-align: left;
-    }
-
-
 </style>
