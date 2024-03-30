180d15476a1b3d87c2985b908375eddd5a51d53a
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-03-30 15:31:08 +0100

    ~ the constructors for arktype based objects will now report errors if there are some

--- frontend/src/lib/stores.svelte.ts
+++ frontend/src/lib/stores.svelte.ts
@@ -1,4 +1,5 @@
 import { ApplicationState, BackupInfosListDef, DEFAULT_FRONTENDCFG, FrontendCfgDef, type BackupInfosList, type FrontendCfg } from '$lib/types';
+import type { CheckResult } from 'arktype/internal/traverse/traverse.js';
 
 function getItem(key: string): string | null {
     if (typeof localStorage === 'undefined') {
@@ -31,7 +32,7 @@ function createStore<T>(key: string, defaultValue: T, deserializer: Deserializer
 
     let result = $state(serialized || defaultValue);
     let changed = $state(false);
-    
+
     function setValue(newValue: T) {
         if (newValue == undefined || newValue == null) {
             removeItem(key);
@@ -56,10 +57,29 @@ function createStore<T>(key: string, defaultValue: T, deserializer: Deserializer
 
 }
 
+function newObject<T>(obj: string, defVal: T, constructor: (value: any) => CheckResult<T>): T {
+    const parsed = JSON.parse(obj);
+    const result = constructor(parsed);
+    if (result.problems) {
+        console.error(`Error Input:\n${obj}`);
+        console.error(JSON.stringify(result.problems));
+        return defVal;
+    }
+    return result.data;
+}
+
+function buildBackupInfosList(input: string): BackupInfosList {
+    return newObject<BackupInfosList>(input, [], BackupInfosListDef);
+}
+
+function buildFrontendCfgt(input: string): FrontendCfg {
+    return newObject<FrontendCfg>(input, DEFAULT_FRONTENDCFG, FrontendCfgDef);
+}
+
 const alertStore = createStore<string>('alertState', '');
 const applicationStateStore = createStore<ApplicationState>('applicationState', ApplicationState.Display);
-const backupinfosStore = createStore<BackupInfosList>('backupInfos', [], (input) => BackupInfosListDef(input).data);
-const configStore = createStore<FrontendCfg>('frontend', DEFAULT_FRONTENDCFG, (input) => FrontendCfgDef(input).data )
+const backupinfosStore = createStore<BackupInfosList>('backupInfos', [], buildBackupInfosList);
+const configStore = createStore<FrontendCfg>('frontend', DEFAULT_FRONTENDCFG, buildFrontendCfgt);
 const progressStore = createStore<number>('progress', DEFAULT_FRONTENDCFG.timeout);
 
 export { alertStore, applicationStateStore, backupinfosStore, configStore, progressStore };
