54c514b2bf51253cf940ba0a07808584babc8748
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-03-30 18:01:55 +0100

    ~ changed to auto adapter as we're using dynamic routes per backup information

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -23,6 +23,7 @@ const config = {
             '$assets': './src/assets',
             '$src': './src'
         }
+
     }
 };
 
